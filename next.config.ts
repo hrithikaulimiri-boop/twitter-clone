73c34f1bc1bf23bf26443b95141e420264651c3e
Hrithika <hrithika@Hrithikas-MacBook-Air.local>
2026-01-18 08:55:53 +0530

    Fix next.config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
 export default nextConfig;
+
+
